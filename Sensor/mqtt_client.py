70b323c212f6f68f3ad4ef2abb3b729ede505af6
HannesLinuxMintVM2 <schuckh.tin22@student.dhbw-heidenheim.de>
2024-09-27 13:13:13 +0200

    added esp32 publsher client

--- Sensor/mqtt_client.py
+++ Sensor/mqtt_client.py
@@ -1,40 +1,44 @@
 from umqtt.simple import MQTTClient
 import ubinascii
 import os
-from parser import decode
+from parser import encode
 import _thread
 import web_server
+import time
+import random
 
 client_id = ubinascii.hexlify(os.urandom(6)).decode('utf-8')
 print("Client-ID:", client_id)
 
-def handle_callback(topic, msg):
-    decoded = decode(msg)
-    if(decoded["Payload"]["Type"] == "Command"):
-        web_server.message = decoded["Payload"]["Value"] #TODO schöner
-    else:
-        web_server.message = "Type wasn't a Command: \n" + str(decoded)
-
-def mqtt_connect_sub(topic, broker_address, broker_port, user, password):
+def mqtt_connect_pub(topic, broker_address, broker_port, user, password):
     client = MQTTClient(client_id, broker_address, port=broker_port, user=user, password=password)
-    client.set_callback(handle_callback)
     client.connect()
-    client.subscribe(topic)
-    print(f"Subscribed to topic: {topic}")
     return client
 
 def mqtt_client(topic, broker_address, broker_port, user, password):
     try:
-        client = mqtt_connect_sub(topic, broker_address, broker_port, user, password)
+        client = mqtt_connect_pub(topic, broker_address, broker_port, user, password)
     except OSError as error:
         print(f"Error connecting to broker: {error}")
         return
 
     _thread.start_new_thread(web_server.web_server, ())
 
-    try:
-        while True:
-            client.check_msg()
-    except KeyboardInterrupt:
-        print("Disconnecting...")
-        client.disconnect()
+    while (True):
+        random_on = random.randint(0, 1)
+
+        header_data = {
+            "Sender": "ESP32-Sensor",
+            "Topic": "actor_heat/topic"
+        }
+
+        value = "ON" if random_on == 1 else "OFF"
+
+        payload_data = {
+            "Type": "Command",
+            "Value": value 
+        }
+
+        message = encode(header_data, payload_data)
+        client.publish(topic, message)
+        time.sleep(5)
